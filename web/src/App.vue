56e3189b34473411b5a9e4b2eb90b975e1ade85c
yjl <3505981550@qq.com>
2024-08-15 19:31:58 +0800

    项目骨架

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+<div>Bot昵称:{{bot_name}}</div>
   <router-view/>
 </template>
-
+<script>
+  import $ from 'jquery';
+  import {ref} from 'vue';
+  export default{
+    name:"APP",
+    setup:()=>{
+      let bot_name = ref("");
+      $.ajax({
+        url:"http://127.0.0.1:8888/pk/getbotinfo/",
+        type:"get",
+        success:resp =>{
+         bot_name.value = resp;
+        }
+      });
+      return{
+        bot_name
+      } 
+    }
+  }
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
